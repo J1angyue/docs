87ca55067f3747ec7392023c2a87454899525795
马振轩 <mzx23333@163.com>
2023-10-04 19:42:19 +0800

    update favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,14 @@
 import { defineUserConfig } from 'vuepress'
 import themeConfig from './themeConfig'
 
+const BASE = '/docs/'
+
 export default defineUserConfig({
   lang: 'zh-CN',
   title: 'J1angyue Docs',
-  base: '/docs/',
+  base: BASE,
   head: [
-    ['link', { rel: 'icon', href: '/logo.png' }]
+    ['link', { rel: 'icon', href: BASE + 'logo.png' }]
   ],
   theme: themeConfig,
 })
